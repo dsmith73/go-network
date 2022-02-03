5b326c2f1fe076f848b75aeee102d7273572ea65
Dan <44279121+dsmith73@users.noreply.github.com>
2022-02-03 09:50:20 -0500

    Update test.go

--- network/test/test.go
+++ network/test/test.go
@@ -10,7 +10,7 @@ func main() {
 	t1 := "cnn.com"
 	t2 := "abc.123.xyz"
 
-	fmt.Println(dns.Lookup(t1))    // Pass - Array returned
-	fmt.Println(dns.Lookup(t1)[0]) // Pass - 1st postion printed
-	fmt.Println(dns.Lookup(t2))    // Fail - no such host
+	fmt.Println(dns.WebLookup(t1))    // Pass - Array returned
+	fmt.Println(dns.WebLookup(t1)[0]) // Pass - 1st IP printed
+	fmt.Println(dns.WebLookup(t2))    // Fail - no such host
 }
